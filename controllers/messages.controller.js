30779ff03e0edf73f357a59673ab15ec70b49aab
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-06-08 23:33:29 +0100

    using template engine successfully

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 function getMessages(req, res) {
-  res.send("<ul><li>Helloo Albert!</li></ul>");
+  res.sendFile(path.join(__dirname, "..", "public", "img", "skimountain.jpg"));
+  // res.send("<ul><li>Helloo Albert!</li></ul>");
 }
 //use named fn, if something went wrong node will be able to tell us the name of fn compared to using fn exp + arrow fns. extra debugging benefit
 
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const friendsRouter = require("./routes/friends.router");
 const messagesRouter = require("./routes/messages.router");
 
 const app = express();
-
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 const PORT = 3000;
 
 //middleware fn
@@ -13,10 +15,17 @@ app.use((req, res, next) => {
   next();
   //actions go here
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta}ms`);
+  console.log(`${req.method} ${req.baseUrl}/${req.url} ${delta}ms`);
 });
+app.use("/site", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
+app.use("/", (req, res) => {
+  res.render("index", {
+    title: "My Friends are all stupid",
+    caption: "Let's; go skiing! Whoopee",
+  });
+});
 app.use("/friends", friendsRouter);
 app.use("/messages", messagesRouter);
 
